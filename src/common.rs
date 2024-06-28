685096703dadd3d317c5d531899127759b3f7bf5
灰太狼大叔 <146620104+MrChenhtlss@users.noreply.github.com>
2024-06-29 00:25:46 +0800

    commit

--- src/common.rs
+++ src/common.rs
@@ -1083,12 +1083,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk.htlss.cn".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("rustdesk.htlss.cn") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
