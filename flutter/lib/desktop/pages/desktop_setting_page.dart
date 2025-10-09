0a60d66271ab365a1303453b283eb1f4d3567cbd
灰太狼大叔 <146620104+MrChenhtlss@users.noreply.github.com>
2025-10-09 17:54:13 +0800

    开源项目

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2267,7 +2267,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('Privacy Statement');
                   },
                   child: Text(
                     translate('Privacy Statement'),
--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -862,7 +862,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             SettingsTile(
               title: Text(translate("Privacy Statement")),
               onPressed: (context) =>
-                  launchUrlString('https://rustdesk.com/privacy.html'),
+                  launchUrlString('https://rustdesk.com/?lang=en'),
               leading: Icon(Icons.privacy_tip),
             )
           ],
