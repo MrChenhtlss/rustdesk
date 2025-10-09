40143068d2ffedeb795d378b7cdbc0ee780fe13a
灰太狼大叔 <146620104+MrChenhtlss@users.noreply.github.com>
2025-10-09 15:53:50 +0800

    commit

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2267,7 +2267,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('https://rustdesk.com');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -2275,7 +2275,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://www.htlss.cn');
                   },
                   child: Text(
                     translate('Website'),
@@ -2293,7 +2293,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Email: chen1350962574@Gmail.com | 1350962574@QQ.com',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
