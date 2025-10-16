d27b50d4e5ff0c4014ec713b7e100773f79bd578
MrChenhtlss <chen20030410@gmail.com>
2025-10-17 02:10:11 +0800

    添加网站提示信息

--- src/lang/en.rs
+++ src/lang/en.rs
@@ -258,5 +258,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("websocket_tip", "When using WebSocket, only relay connections are supported."),
         ("terminal-admin-login-tip", "Please input the administrator username and password of the controlled side."),
         ("elevation_username_tip", "Input username or domain\\username"),
+        ("Website", "Online Files"),
     ].iter().cloned().collect();
 }
